3b36c03764240d57f411c46698b7d802a81f2031
SURFACE7PLUS\BillA <BillA@flosim.com>
2024-04-05 14:25:36 +0100

    Cleaned up repo

--- hoisting.js
+++
@@ -1,9 +0,0 @@
-function f() {
-  console.log("v = ", v);
-  v = 35;
-  console.log("v = ", v);
-  let v = 45;
-  console.log("v = ", v);
-}
-
-f();
--- test.js
+++
@@ -1,5 +0,0 @@
-element.addEventListener("transitionend", (event) => {
-  let propertyName = event.propertyName;
-  let elapsedTime = event.elapsedTime;
-  /* do something here… */
-}, true);
